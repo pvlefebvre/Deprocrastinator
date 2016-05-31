6819f715b329ecd901cae9db493a286eb70d42c1
Paul Lefebvre <pvl4j@virginia.edu>
2016-05-31 14:07:30 -0500

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by Paul Lefebvre on 5/31/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Paul Lefebvre on 5/31/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
