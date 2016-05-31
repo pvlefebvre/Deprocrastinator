65da564a36f30a317dd63ff7be2b1fb4187981fd
Lance Russ <lance.r.russ@gmail.com>
2016-05-31 15:38:42 -0500

    working

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController () <UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *items;
 
 @end
 
@@ -18,19 +19,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.items = [NSMutableArray new];
     
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    cell.textLabel.text = [self.items objectAtIndex:indexPath.row];
     return cell;
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 4;
-}
-- (IBAction)onAddButtonPressed:(id)sender {
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+        [tableView cellForRowAtIndexPath:indexPath].textLabel.textColor = [UIColor greenColor];
     
+}
+- (IBAction)onRowItemSwiped:(UISwipeGestureRecognizer *)sender {
+    
+    
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.items.count;
+}
+
+- (IBAction)onAddButtonPressed:(id)sender {
+    [self.items addObject:self.textField.text];
+    [self.tableView reloadData];
+    self.textField.text = @"";
+    [self.textField resignFirstResponder];
+}
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+
+    if (self.editing) {
+        self.editing = false;
+        [self.tableView setEditing:false animated:true];
+        [sender setStyle:UIBarButtonItemStylePlain];
+        [sender setTitle:@"Edit"];
+    } else {
+        self.editing = true;
+        [self.tableView setEditing:true animated:true];
+        [sender setStyle:UIBarButtonItemStyleDone];
+        [sender setTitle:@"Done"];
+    }
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.items removeObjectAtIndex:indexPath.row];
+    [self.tableView reloadData];
 }
 
 @end
